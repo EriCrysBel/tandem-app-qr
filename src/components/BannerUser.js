cde3bbe184eac136782a92b33995a338e1784899
EriCrysBel <erikacrystal95@gmail.com>
2024-08-02 09:51:28 +0200

    optimizacion para subir a netlify

--- src/components/BannerUser.js
+++ src/components/BannerUser.js
@@ -1,12 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ImageContext } from '../context/ImageContext';
 import './BannerUser.css';
 
 const BannerUser = ({ darkMode }) => {
-  const { userImageUrl } = useContext(ImageContext);
-  const userName = typeof window !== 'undefined' ? localStorage.getItem('tandem_nombre') : '';
+  const context = useContext(ImageContext);
+  const [userName, setUserName] = useState('');
+  const [userImageUrl, setUserImageUrl] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setUserName(localStorage.getItem('tandem_nombre'));
+      if (context) {
+        setUserImageUrl(context.userImageUrl);
+      }
+    }
+  }, [context]);
 
   const rutaimg = `https://erika.tandempatrimonionacional.eu/gatsbyqr/images/users/${userImageUrl}`;
+
+  if (typeof window === 'undefined') {
+    // Render something else during SSR
+    return null;
+  }
 
   return (
     <div className='banneruser' style={{ color: darkMode ? 'white' : 'black' }}>
